de4daedcfc272ef1660357e4083056c35487e671
weiweirj <weiweirj@126.com>
2021-06-18 16:23:49 +0800

    更新内容： 1.系统设置内顶部不再显示更新提示 2.缩小了apk大小，82.34 KB--》6.1 KB

--- app/src/main/java/com/oneplus7Pro/banUpdate/Main.java
+++ app/src/main/java/com/oneplus7Pro/banUpdate/Main.java
@@ -1,5 +1,7 @@
 package com.oneplus7Pro.banUpdate;
 
+import android.util.Log;
+
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
@@ -8,13 +10,21 @@ import de.robv.android.xposed.callbacks.XC_LoadPackage;
 public class Main implements IXposedHookLoadPackage{
     @Override
     public void handleLoadPackage(XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable {
-        if (loadPackageParam.packageName.equals("com.oneplus.opbackup")){
-            XposedHelpers.findAndHookMethod(loadPackageParam.classLoader.loadClass("org.json.JSONObject"), "getString",String.class, new XC_MethodHook() {
+        if (loadPackageParam.packageName.equals("com.oneplus.opbackup")) {
+            XposedHelpers.findAndHookMethod(loadPackageParam.classLoader.loadClass("org.json.JSONObject"), "getString", String.class, new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    if (param.args[0]=="new_version" || param.args[0]=="down_url" )
+                    if (param.args[0] == "new_version" || param.args[0] == "down_url")
                         param.setResult("");
                     super.afterHookedMethod(param);
+                }
+            });
+        }
+        if (loadPackageParam.packageName.equals("com.android.settings")) {
+            XposedHelpers.findAndHookMethod(loadPackageParam.classLoader.loadClass("com.android.settings.homepage.contextualcards.conditional.OpOtaConditionController"), "isDisplayable", new XC_MethodHook() {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    param.setResult(false);
                 }
             });
         }
